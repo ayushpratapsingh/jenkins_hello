58cf74d02da0d23e2dab0ce4943ec2027489a12e
ayushpratapsingh <ayush.pratap906@gmail.com>
2024-11-16 12:41:32 +0530

    Update HelloWorld_webhook.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("-----***** Trying Webhook *****-----");
 		System.out.println("------****** Welcome to Jenkins ******------");
+		System.out.println("------****** Welcome to Jenkins by Git webhook ******------");
 	}
 }
